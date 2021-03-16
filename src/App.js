57ef03242f930cf4158b46075987d56c30270b31
PraneshAllena <praneshallena@gmail.com>
2021-03-16 19:09:09 +0530

    public test

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ class App extends Component {
         <Row className='p-2'>
           <Container>
             <Col>
-            <h1 className='text-white'>Monsters Rolodex</h1>
+            <h1 className='text-white'>testing public branch</h1>
             </Col>
             <Col>
               <SearchBox
